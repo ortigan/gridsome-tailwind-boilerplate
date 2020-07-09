9a26ccd167c735cd965550f3a42cbea8d0197efe
Parag Chirde <parag.chirde@gmail.com>
2020-07-09 12:08:17 +0530

    Initial Commit - Empty Project

---
+++ gridsome.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    siteName: 'MGM IBT',
+    plugins: []
+}
---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <Layout>
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+
+    <h1>Hello, world!</h1>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
